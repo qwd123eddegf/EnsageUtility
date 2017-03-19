9ffade9ffa128d11b238ed2d5e7232797f8129c6
MaggusLakers <imeh@protonmail.com>
2017-03-19 16:42:43 +0100

    Published uStash & uCamera

---
+++ uCamera/uCamera/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Ensage;
+using Ensage.Common;
+using Ensage.Common.Menu;
+
+namespace uCamera
+{
+    class Program
+    {
+        private static Menu _menu;
+
+        static void Main(string[] args)
+        {
+            Events.OnLoad += Events_OnLoad;
+        }
+
+        private static void Events_OnLoad(object sender, EventArgs e)
+        {
+            _menu = new Menu("Utility Camera", "uCam", true);
+
+            var overwatch = _menu.AddSubMenu(new Menu("Overwatch", "overwatch"));
+            overwatch.AddItem(new MenuItem("enable", "Enable Overwatch").SetValue(false));
+            overwatch.AddItem(new MenuItem("enabletop", "Top Runespot").SetValue(false));
+            overwatch.AddItem(new MenuItem("enablebot", " Bot Runespot").SetValue(false));
+            overwatch.AddItem(new MenuItem("enabletbase", "Home Base").SetValue(false));
+            overwatch.AddItem(new MenuItem("enableebase", "Enemy Base").SetValue(false));
+
+            _menu.AddItem(new MenuItem("lockCamera", "Lock Camera").SetValue(new KeyBind('Z', KeyBindType.Toggle))).ValueChanged += Lock_Changed;
+
+            _menu.AddToMainMenu();
+        }
+
+        private static void Lock_Changed(object sender, OnValueChangeEventArgs e)
+        {
+            Game.ExecuteCommand($"dota_camera_lock {Convert.ToInt32(e.GetNewValue<KeyBind>().Active)}");
+        }
+    }
+}
---
+++ uStash/uStash/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using Ensage;
+using Ensage.Common;
+using Ensage.Common.Menu;
+using Ensage.Common.Threading;
+
+namespace uStash
+{
+    internal class Program
+    {
+        private static Hero _me;
+        private static Menu _menu;
+
+        static void Main(string[] args)
+        {
+            Events.OnLoad += Events_OnLoad;
+            Events.OnClose += Events_OnClose;
+        }
+
+        private static void Events_OnLoad(object sender, EventArgs e)
+        {
+            _me = ObjectManager.LocalHero;
+
+            _menu = new Menu("Stash Picker", "StashPicker", true);
+            _menu.AddItem(new MenuItem("hotkeyPick", "Hotkey to pick").SetValue(new KeyBind('I', KeyBindType.Press)));
+            _menu.AddItem(new MenuItem("alwaysPick", "Always pick").SetValue(false));
+
+            _menu.AddToMainMenu();
+
+            GameDispatcher.OnIngameUpdate += GameDispatcher_OnIngameUpdate;
+        }
+
+        private static void Events_OnClose(object sender, EventArgs e)
+        {
+            GameDispatcher.OnIngameUpdate -= GameDispatcher_OnIngameUpdate;
+        }
+
+        private static void PickfromStash()
+        {
+            var stash = _me.Inventory.Stash;
+
+            if (!stash.Any())
+                return;
+
+            foreach (var item in stash)
+            {
+                item.MoveItem(_me.Inventory.FreeSlots.FirstOrDefault());
+            }
+        }
+
+        private static void GameDispatcher_OnIngameUpdate(EventArgs args)
+        {
+
+            if (Game.IsPaused || !_me.IsAlive)
+            {
+                return;
+            }
+
+            if (_me.AvailableShops != ShopFlags.None &&
+                (_menu.Item("hotkeyPick").GetValue<KeyBind>().Active || _menu.Item("alwaysPick").GetValue<bool>()))
+            {
+                PickfromStash();
+            }
+        }
+    }
+}
